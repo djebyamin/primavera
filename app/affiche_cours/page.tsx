0e4065e14a194b1a3616d33ac8a02b97e8be92f4
djebyamin <144284438+djebyamin@users.noreply.github.com>
2024-08-27 11:18:41 +0100

    get authenticate the page **affiche_cours**

--- app/affiche_cours/page.tsx
+++ app/affiche_cours/page.tsx
@@ -1,32 +1,13 @@
-// src/app/courses/page.tsx
-import { PrismaClient } from "@prisma/client";
-import Image from 'next/image'; // Optional if you plan to add course images
+import { redirect } from "next/navigation";
+import { auth } from "@/app/auth";
+import Cours from "@/app/affiche_cours/cours";
 
-const prisma = new PrismaClient();
+export default async function SigninPage() {
+  const session = await auth();
 
-async function getCourses() {
-  // Fetch all courses from the database
-  return await prisma.course.findMany();
-}
+  if (!session) {
+    redirect("/signin");
+  }
 
-export default async function CoursesPage() {
-  const courses = await getCourses(); // Fetch the courses
-
-  return (
-    <div className="min-h-screen bg-gray-100 py-10 px-6">
-      <h1 className="text-4xl font-bold text-center text-gray-800 mb-8">Tous les Cours</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-        {courses.map((course) => (
-          <div key={course.id} className="bg-white rounded-lg shadow-lg overflow-hidden transform hover:scale-105 transition-transform duration-300">
-            <div className="p-6">
-              <h2 className="text-2xl font-semibold text-gray-800 mb-2">{course.nom}</h2>
-              <p className="text-gray-600 mb-4">{course.description}</p>
-              <p className="text-lg font-bold text-indigo-600">Prix: {course.prix}€</p>
-              <button className="mt-4 bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 transition duration-300">ADD this Course</button>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+  return <Cours/>;
 }
